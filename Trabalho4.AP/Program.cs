83d1ca8d684a81ebbce1cbce0e6a3d2e75788065
Iagodizlla <iagoschlemper@gmail.com>
2025-05-12 17:21:44 -0300

    Implementado cadastro de fabricante em html

--- Trabalho4.AP/Program.cs
+++ Trabalho4.AP/Program.cs
@@ -17,17 +17,46 @@ class Program
 
         app.MapGet("/", PaginaInicial);
 
+        app.MapGet("/fabricantes/cadastrar", FormularioCadastrarFabricante);
+        app.MapPost("/fabricantes/cadastrar", CadastrarFabricante);
+
         app.MapGet("/fabricantes/visualizar", VisualizarFabricantes);
 
         app.Run();
+    }
+    static Task CadastrarFabricante(HttpContext context)
+    {
+        string nome = context.Request.Form["nome"].ToString();
+        string email = context.Request.Form["email"].ToString();
+        string telefone = context.Request.Form["telefone"].ToString();
+
+        Fabricante fabricante = new Fabricante(nome, email, telefone);
+
+        ContextoDados contextoDados = new ContextoDados(true);
+        IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
+
+        repositorioFabricante.CadastrarRegistro(fabricante);
+
+        string conteudo = File.ReadAllText("Html/Notificacao.html");
+
+        StringBuilder sb = new StringBuilder(conteudo);
+        sb.Replace("#mensagem#", fabricante.Nome + " cadastrado com sucesso!");
+
+        string conteudoFinal = sb.ToString();
+
+        return context.Response.WriteAsync(conteudoFinal);
+    }
+    static Task FormularioCadastrarFabricante(HttpContext context)
+    {
+        string conteudo = File.ReadAllText("Html/FormularioCadastrarFabricante.html");
+
+
+        return context.Response.WriteAsync(conteudo);
     }
     static Task VisualizarFabricantes(HttpContext context)
     {
         ContextoDados contextoDados = new ContextoDados(true);
         IRepositorioFabricante repositorioFabricante = new RepositorioFabricanteEmArquivo(contextoDados);
-
-        repositorioFabricante.CadastrarRegistro(new Fabricante("dell", "teste@dell.com", "(11) 1111-1111"));
-        repositorioFabricante.CadastrarRegistro(new Fabricante("aple", "teste2@apple.com", "(21) 1111-1111"));
 
         string conteudo = File.ReadAllText("Html/VisualizarFabricantes.html");
 
